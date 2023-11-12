4ea443f1d06e83044f4a47ce40705c84a4bc1574
Krishantchauhan <krashantchauhan@gmail.com>
2023-11-12 11:17:34 +0530

    intro to obj
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

---
+++ 02-Fundamentals-Part-2/10-introduction-to-objects.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// In array the order of elements matter because we access them using their order
+const Krisharr = [
+  'Krishant',
+  'Chauhan',
+  2022 - 1991,
+  'teacher',
+  ['Krishant', 'Krish', 'Iya'],
+];
+
+//Object jonas has 5 property and the order of the property does not matter when we retrieve them
+const Krish = {
+  firstName: 'Krishant',
+  lastName: 'Chauhan',
+  age: 2022 - 1991,
+  job: 'teacher',
+  friends: ['Krishant', 'Krish', 'Iya'],
+};
